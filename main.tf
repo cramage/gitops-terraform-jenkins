d184063630380d8f8a42a4bb8d8e5fd4c782a4ab
cramage <35350856+cramage@users.noreply.github.com>
2020-07-11 17:20:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "default" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["5.68.215.198/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
